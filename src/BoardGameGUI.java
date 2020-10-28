3485ec73902a8ef8135c9ecab527e580620e5bb1
peteralmgren <peter.almgren78@icloud.com>
2020-10-28 14:49:22 +0100

    new master

--- src/BoardGameGUI.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Peter Almgren, Nick Danielsson
- * Date: 2020-10-21
- * Time: 17:00
- * Project: JAVASprint3
- * Copyright: MIT
- */
-public class BoardGameGUI {
-}
--- src/BoardGameLogic.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Peter Almgren,Nick Danielsson
- * Date: 2020-10-22
- * Time: 09:16
- * Project: JAVASprint3
- * Copyright: MIT
- */
-public class BoardGameLogic {
-}
